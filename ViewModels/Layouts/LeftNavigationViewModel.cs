dd2e56bd441a5d25e62a2bae00592764b8fa1c3f
NHLAMULO-REPLY <shikweninhlamulo@gmail.com>
2024-09-08 20:00:20 +0200

    merge

---
+++ ViewModels/Layouts/LeftNavigationViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CLA_Administration_Web.Helpers.Enums.Shared;
+
+namespace CLA_Administration_Web.ViewModels.Layouts
+{
+    public class LeftNavigationViewModel
+    {
+        public List<ModuleNameAndPagesViewModel> ModuleNamesPages { get; set; }
+
+        public List<SettingsNamesType> SettingsNamesType { get; set; }
+    }
+}
