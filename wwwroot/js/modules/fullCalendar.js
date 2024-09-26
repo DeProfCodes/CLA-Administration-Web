0f23c76c5758e500edb5cb4a2a246a1b7fcb49e0
Proficient Mkansi <de.prof.codes@gmail.com>
2024-09-26 12:06:01 +0200

    - calendar view

--- wwwroot/js/modules/fullCalendar.js
+++ wwwroot/js/modules/fullCalendar.js
@@ -7,11 +7,11 @@ function CreateModuleCalendarView(calendarDivId,eventsData)
     var calendar = new FullCalendar.Calendar(calendarEl, 
     {
         initialView: 'timeGridWeek',
-        initialDate: '2024-08-07',
+        initialDate: TodayDate(),
         headerToolbar: {
           left: 'prev,next today',
           center: 'title',
-          right: 'timeGridWeek,timeGridDay'
+          right: 'dayGridMonth,timeGridWeek,timeGridDay'
         },
         events: eventsData
     });
