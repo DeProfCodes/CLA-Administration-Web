52555fbc3d367e310e3022075cff274455860024
DeProfCodes <de.prof.codes@gmail.com>
2024-12-02 23:23:51 +0200

    Started with Staging Users

--- Helpers/Enums/AppPages/SettingsPages.cs
+++ Helpers/Enums/AppPages/SettingsPages.cs
@@ -4,34 +4,40 @@ namespace CLA_Administration_Web.Helpers.Enums.AppPages
 {
     public enum SettingsPages
     {
-        [Display(Name = "")]
+        [Display(Name = "", Description = "")]
         None,
 
-        [Display(Name = "StagingUsers")]
+        [Display(Name = "AllSettings", Description = "All Settings")]
+        AllSettings,
+
+        [Display(Name = "StagingUsers", Description = "Staging Users")]
         StagingUsers,
 
-        [Display(Name = "SetupExclusions")]
+        [Display(Name = "AddNewStagingUser", Description = "Add New Staging User")]
+        AddNewStagingUser,
+
+        [Display(Name = "SetupExclusions", Description = "Setup Exclusions")]
         SetupExclusions,
 
-        [Display(Name = "AdminAccess")]
+        [Display(Name = "AdminAccess", Description = "Admin Access")]
         AdminAccess,
 
-        [Display(Name = "TargetGroups")]
+        [Display(Name = "TargetGroups", Description = "Target Groups")]
         TargetGroups,
 
-        [Display(Name = "CustomUserSettings")]
+        [Display(Name = "CustomUserSettings", Description = "Custom User Settings")]
         CustomUserSettings,
 
-        [Display(Name = "ActiveConnections")]
+        [Display(Name = "ActiveConnections", Description = "Active Connections")]
         ActiveConnections,
 
-        [Display(Name = "SkinsOfflineImages")]
+        [Display(Name = "SkinsOfflineImages", Description = "Skins And Offline Images")]
         SkinsOfflineImages,
 
-        [Display(Name = "DefaultFonts")]
+        [Display(Name = "DefaultFonts", Description = "Default Fonts")]
         DefaultFonts,
 
-        [Display(Name = "DesktopInformation")]
+        [Display(Name = "DesktopInformation", Description = "Desktop Information")]
         DesktopInformation
     }
 }
---
+++ ViewModels/Settings/SettingsBreadcrumbViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CLA_Administration_Web.Helpers.Enums.AppPages;
+
+namespace CLA_Administration_Web.ViewModels.Settings
+{
+    public class SettingsBreadcrumbViewModel
+    {
+        public SettingsPages SettingsCurrentPage { get; set; }
+
+        public bool HasAddNewButton { get; set; }
+
+        public SettingsPages AddNewPage { get; set; }
+    }
+}
