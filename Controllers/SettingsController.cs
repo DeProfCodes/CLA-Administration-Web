57bbde3eb79c45fed3358c1f99b0029273d675c2
LebogangC <lebogangchibwana40@gmail.com>
2024-12-11 23:39:11 +0200

    Active connections and Admin Access additions

--- Controllers/SettingsController.cs
+++ Controllers/SettingsController.cs
@@ -1,6 +1,8 @@
 using CLA_Administration_Web.Helpers.Constants;
 using CLA_Administration_Web.Helpers.MockData;
 using CLA_Administration_Web.ViewModels.Settings;
+using CLA_Administration_Web.ViewModels.Settings.ActiveConections;
+using CLA_Administration_Web.ViewModels.Settings.ActiveConnections;
 using CLA_Administration_Web.ViewModels.Settings.SetupExclusion;
 using CLA_Administration_Web.ViewModels.Settings.StagingUsers;
 using Microsoft.AspNetCore.Mvc;
@@ -40,6 +42,7 @@ namespace CLA_Administration_Web.Controllers
             return PartialView(AppPagesLinks.Settings.SetupExclusionsPageLink, setupExcludedData);
         }
 
+
         [HttpGet]
         public async Task<IActionResult> AdminAccess()
         {
@@ -61,6 +64,12 @@ namespace CLA_Administration_Web.Controllers
         [HttpGet]
         public async Task<IActionResult> ActiveConnections()
         {
+            
+                var connections = new ActiveConnectionsMainViewModel()
+                {
+                    Connections = SettingsMockData.ActiveConnections,
+                };
+        
             return PartialView(AppPagesLinks.Settings.ActiveConnectionsPageLink);
         }
 
