8ded61505b165266dbf036b9ef95c5d551f1c7b0
NHLAMULO-REPLY <shikweninhlamulo@gmail.com>
2025-01-15 23:47:43 +0200

    change target group filter

--- ViewModels/Settings/ManageAdminAccess/ManageAdminAccessMainViewModel.cs
+++ ViewModels/Settings/ManageAdminAccess/ManageAdminAccessMainViewModel.cs
@@ -1,7 +1,14 @@
-﻿namespace CLA_Administration_Web.ViewModels.Settings.ManageAdminAccess
+﻿using CLA_Administration_Web.ViewModels.Targeting;
+
+namespace CLA_Administration_Web.ViewModels.Settings.ManageAdminAccess
 {
     public class ManageAdminAccessMainViewModel
     {
         public List<AdminAccessItem> AdminAccessItems { get; set; }
+        public List<TargetedEntityTree> TargetedEntities { get; set; }
+
+        public List<TargetedEntityTree> TargetedAccepted { get; set; }
+
+        public List<TargetedEntityTree> TargetedGroups { get; set; }
     }
 }
